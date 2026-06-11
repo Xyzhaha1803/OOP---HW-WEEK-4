bf162bc7eb531143d58ddebaea2f6853362ff7e5
Xyzhaha1803 <officalsahil@gmail.com>
2026-06-11 21:54:51 +0545

    partial Q2

---
+++ Q2/BankSystem.java
@@ -0,0 +1,64 @@
+/* Create a class BankAccount with:
+Fields: accountNumber, balance
+Methods: deposit(double amount), withdraw(double amount), and getBalance()
+Create a subclass SavingsAccount with:
+Field: interestRate
+Override withdraw() to prevent withdrawal if it causes the balance to fall below a threshold (e.g., 1000).
+Add a method addInterest() to update balance based on the interest rate.
+Make all fields private and use getters/setters.
+Demonstrate account creation, deposit, interest addition, and controlled withdrawal.
+File: BankSystem.java. */
+
+class BankAccount{
+    private int accountNumber;
+    private double balance;
+
+    public void setAccountNumber(int AN){
+        this.accountNumber = AN;
+    }
+
+    public int getAcountNumber(){
+        return accountNumber;
+    }
+
+    public void setBalance(double b){
+        this.balance = b;
+    }
+
+    public double getBalance(){
+        return balance;
+    }
+
+    void deposit(double amount){
+        this.balance += amount;
+    }
+
+    void withdraw(double amount){
+        this.balance -= amount;
+    }
+}
+
+class SavingsAccount extends BankAccount{
+    private double interestRate;
+
+    public void setInterestRate(double IR){
+        this.interestRate = IR;
+    }
+
+    public double getInterestRate(){
+        return interestRate;
+    }
+
+    @Override
+    void withdraw(double amount){
+        if (getBalance() <= 0){
+            System.out.println("Not enough balance.");
+        } else {
+            setBalance(getBalance() - amount);
+        }
+    }
+}
+
+public class BankSystem {
+    
+}
